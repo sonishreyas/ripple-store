3c6497d7e3286d568cfce332135e4f1ac4cbaaf5
Shreyas Soni <sonishreyas10@gmail.com>
2022-02-22 02:53:21 +0530

    feat: responsiveness added

--- main/js/handle-nav-bar.js
+++ main/js/handle-nav-bar.js
@@ -19,27 +19,10 @@ const handleNavBar = () => {
 		brandName.style.visibility = "visible";
 	});
 	window.addEventListener("resize", function () {
-		if (window.innerWidth >= 1180) {
-			navBar.style.visibility = "visible";
-			brandName.style.visibility = "visible";
-			if (drawerContent.classList.contains("display-inactive") === false) {
-				drawerContent.classList.add("display-inactive");
-			}
-		} else {
-			if (drawerContent.classList.contains("display-inactive")) {
-				drawerContent.classList.remove("display-inactive");
-			}
-			navBar.style.visibility = "hidden";
-			drawerContent.style.visibility = "hidden";
-			brandName.style.visibility = "visible";
-		}
+		navBar.style.visibility = "hidden";
+		drawerContent.style.visibility = "hidden";
+		brandName.style.visibility = "visible";
 	});
-
-	if (window.innerWidth < 1180) {
-		if (drawerContent.classList.contains("display-inactive")) {
-			drawerContent.classList.remove("display-inactive");
-		}
-	}
 };
 
 export { handleNavBar };
