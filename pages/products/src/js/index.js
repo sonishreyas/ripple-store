3634c1b479631059f02e179e0bd73bd70841fde2
Shreyas Soni <sonishreyas10@gmail.com>
2022-02-21 20:46:36 +0530

    ref: filter refactored

--- pages/products/src/js/index.js
+++ pages/products/src/js/index.js
@@ -1,8 +1,10 @@
 // // Index JS
 import { handleNavBar } from "../../../../main/js/handle-nav-bar.js";
-
+import { handleRangeSlider } from "./range-slider.js";
 // runs all the js files
 const runScripts = () => {
 	handleNavBar();
+	handleRangeSlider("price-slider", "100");
+	handleRangeSlider("rating-slider", "1");
 };
 runScripts();
